b13d7559a54ea7a5b67bab54e8eafeac5d9ea9c0
Tsunami <tsunami6997@gmai.com>
2021-12-16 10:15:03 +0800

    create wl_display,wl_event_loop,wlr_backend, but include error

--- compositor-demo.c
+++ compositor-demo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //wlroot headers
-// #include <wlr/backend.h>
+#include <wlr/backend.h>
 
 //wayland protocol
 #include <wayland-server.h>
@@ -10,6 +10,7 @@ struct mcv_server {
     struct wl_display *wl_display;
     struct wl_event_loop *wl_event_loop;
 
+    struct wlr_backend *backend;
 };
 
 int main(int argc, char **argv)
@@ -18,6 +19,16 @@ int main(int argc, char **argv)
 
     server.wl_display = wl_display_create();
     server.wl_event_loop = wl_display_get_event_loop(server.wl_display);
+
+    server.backend = wlr_backend_autocreate(server.wl_display);
+    if (!wlr_backend_start(server.backend)) {
+        fprintf(stderr, "Failed to start backedn\n");
+        wl_display_destroy(server.wl_display);
+        return 1;
+    }
+
+    wl_display_run(server.wl_display);
+    wl_display_destroy(server.wl_display);
 
     return 0;
 }
